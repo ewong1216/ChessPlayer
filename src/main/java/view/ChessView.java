0a71b0aa3944f198f3d4bc0ffa70064f295c0b33
Kevin <kevindpaulsen@gmail.com>
2020-12-02 21:37:19 -0800

    Added drag and drop support to pieces.

--- src/main/java/view/ChessView.java
+++ src/main/java/view/ChessView.java
@@ -1,18 +1,22 @@
 package view;
 
-import model.ChessBoard;
 import model.Square;
 
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class ChessView extends JFrame {
 
+    private static final int STARTING_SIZE = 500;
+
     private final ChessBoardView boardView;
 
-    public ChessView(Square[][] board) {
-        boardView = new ChessBoardView(board);
+    public ChessView(Square[][] board, MouseListener mouseListener,
+                     MouseMotionListener motionListener) {
+        boardView = new ChessBoardView(board, mouseListener, motionListener);
         init();
         this.add(boardView, BorderLayout.CENTER);
     }
@@ -21,7 +25,7 @@ public class ChessView extends JFrame {
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        this.getContentPane().setPreferredSize(new Dimension(500, 500));
+        this.getContentPane().setPreferredSize(new Dimension(STARTING_SIZE, STARTING_SIZE));
         this.pack();
     }
 }
