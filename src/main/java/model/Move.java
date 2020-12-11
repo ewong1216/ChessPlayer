90778f42490c3f8f43811fc4921ed5171142e7fe
Ethan Wong <ewong1216@hotmail.com>
2020-12-10 16:19:17 -0800

    added isPromotion to Move called constructor with isPromotion (ChessBoard line 287)

--- src/main/java/model/Move.java
+++ src/main/java/model/Move.java
@@ -9,14 +9,21 @@ public class Move {
     private final ChessCoordinate startCoordinate;
     private final ChessCoordinate endCoordinate;
     private final ChessCoordinate capturedStartCoordinate;
+    private final boolean isPromotion;
 
     public Move(ChessPiece movingPiece, ChessPiece capturedPiece, ChessCoordinate startCoordinate,
                 ChessCoordinate endCoordinate, ChessCoordinate capturedStartCoordinate) {
+        this(movingPiece,capturedPiece,startCoordinate,endCoordinate,capturedStartCoordinate, false);
+    }
+
+    public Move(ChessPiece movingPiece, ChessPiece capturedPiece, ChessCoordinate startCoordinate,
+                ChessCoordinate endCoordinate, ChessCoordinate capturedStartCoordinate, boolean isPromotion) {
         this.movingPiece = movingPiece;
         this.capturedPiece = capturedPiece;
         this.startCoordinate = startCoordinate;
         this.endCoordinate = endCoordinate;
         this.capturedStartCoordinate = capturedStartCoordinate;
+        this.isPromotion = isPromotion;
     }
 
     public ChessPiece getMovingPiece() {
@@ -41,5 +48,9 @@ public class Move {
 
     public boolean doesCapture() {
         return capturedPiece != null;
+    }
+
+    public boolean getIsPromotion(){
+        return isPromotion;
     }
 }
